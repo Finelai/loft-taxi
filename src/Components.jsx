2cad28bb54c63485a8d718c1ade86189dab4f4bb
Finelai <5221032+Finelai@users.noreply.github.com>
2021-01-13 13:38:17 +0300

    add component

---
+++ src/Components.jsx
@@ -0,0 +1,11 @@
+const Header = () => <>{/* Some Header Layout */}</>;
+
+const LoginForm = () => <>{/* Login Form Layout */}</>;
+
+const LoginPanel = () => (
+  <div><LoginForm /></div>
+);
+
+const LoginPage = () => (
+  <><Header /><LoginPanel /></> 
+);
