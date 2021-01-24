a25fcfc5b43656590da291ecb647f20639d48ebc
Finelai <5221032+Finelai@users.noreply.github.com>
2021-01-24 21:27:12 +0300

    add testing library

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import { AppBar, Toolbar, Button } from '@material-ui/core';
-import { Logo } from 'loft-taxi-mui-theme';
+import React from "react";
+import { AppBar, Toolbar, Button } from "@material-ui/core";
+import { Logo } from "loft-taxi-mui-theme";
 
 class Header extends React.Component {
-    navigateTo = (page) => {
-        this.props.changePage(page);
-    };
+  navigateTo = (page) => {
+    this.props.changePage(page);
+  };
 
-    render() {
-        return (
-            <AppBar position="static">
-                <Toolbar>
-                    <Logo></Logo>
-                    <Button color="inherit" onClick={() => this.navigateTo('login')}>Вход</Button>
-                    <Button color="inherit" onClick={() => this.navigateTo('reg')}>Регистрация</Button>
-                    <Button color="inherit" onClick={() => this.navigateTo('map')}>Карта</Button>
-                    <Button color="inherit" onClick={() => this.navigateTo('profile')}>Профиль</Button>
-                </Toolbar>
-            </AppBar>
-        );
-    }
+  render() {
+    return (
+      <AppBar position="static">
+        <Toolbar>
+          <Logo></Logo>
+          <Button color="inherit" onClick={() => this.navigateTo("login")}>
+            Вход
+          </Button>
+          <Button color="inherit" onClick={() => this.navigateTo("reg")}>
+            Регистрация
+          </Button>
+          <Button color="inherit" onClick={() => this.navigateTo("map")}>
+            Карта
+          </Button>
+          <Button color="inherit" onClick={() => this.navigateTo("profile")}>
+            Профиль
+          </Button>
+        </Toolbar>
+      </AppBar>
+    );
+  }
 }
 
 export default Header;
