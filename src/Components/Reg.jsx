71c75c0a95220224a43f8d8d7147df3f8307fc0b
Finelai <5221032+Finelai@users.noreply.github.com>
2021-03-04 15:36:01 +0300

    rewrite reg form to hook form

--- src/Components/Reg.jsx
+++ src/Components/Reg.jsx
@@ -3,62 +3,57 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { userIsLoggedIn, reg } from "modules/user";
+import { useForm } from "react-hook-form";
 
-export class Reg extends React.Component {
-  static propTypes = {
-    isLoggedIn: PropTypes.bool,
-    reg: PropTypes.func
-  };
+const Reg = (props) => {
+  const { register, handleSubmit } = useForm();
 
-  handleRegSubmit = event => {
-    event.preventDefault();
-
-    const name = event.target.firstName.value;
-    const surname = event.target.lastName.value;
-    const email = event.target.lastName.value;
-    const password = event.target.lastName.value;
-
-    const { reg } = this.props;
+  const handleRegSubmit = (data) => {
+    const { name, surname, email, password } = data;
+    const { reg } = props;
     reg({ name, surname, email, password });
   };
 
-  render() {
-    return (
-      <div className="reg">
-        {this.props.isLoggedIn ? (
-          <div>
-            <p>Вы успешно вошли в систему!</p>
-            <Link to="/profile">Перейти в профиль</Link>
-          </div>
-        ) : (
-          <div className="reg__form">
-            <h2>Регистрация</h2>
-            <form onSubmit={this.handleRegSubmit}>
-              <label htmlFor="firstName">
-                Имя:
-                <input name="firstName" type="text" />
-              </label>
-              <label htmlFor="lastName">
-                Фамилия:
-                <input name="lastName" type="text" />
-              </label>
-              <label htmlFor="email">
-                E-mail:
-                <input name="email" type="email" />
-              </label>
-              <label htmlFor="password">
-                Пароль:
-                <input name="password" type="password" />
-              </label>
-              <input type="submit" value="Зарегистрировать" />
-            </form>
-            <p>Уже зарегистрированы? <Link to="/login">Войти</Link></p>
-          </div>
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="reg">
+      {props.isLoggedIn ? (
+        <div>
+          <p>Вы успешно вошли в систему!</p>
+          <Link to="/profile">Перейти в профиль</Link>
+        </div>
+      ) : (
+        <div className="reg__form">
+          <h2>Регистрация</h2>
+          <form onSubmit={handleSubmit(handleRegSubmit)}>
+            <label htmlFor="firstName">
+              Имя:
+              <input name="firstName" type="text" ref={register} />
+            </label>
+            <label htmlFor="lastName">
+              Фамилия:
+              <input name="lastName" type="text" ref={register} />
+            </label>
+            <label htmlFor="email">
+              E-mail:
+              <input name="email" type="email" ref={register} />
+            </label>
+            <label htmlFor="password">
+              Пароль:
+              <input name="password" type="password" ref={register} />
+            </label>
+            <input type="submit" value="Зарегистрировать" />
+          </form>
+          <p>Уже зарегистрированы? <Link to="/login">Войти</Link></p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+Reg.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  reg: PropTypes.func
+};
 
 const mapStateToProps = state => ({
   isLoggedIn: userIsLoggedIn(state)
